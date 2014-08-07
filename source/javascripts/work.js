ceeebe1674f8151100b1ecf8a8b4451a1be2b3f7
Piotr Klosinski <mybettingstuff@gmail.com>
2014-08-07 16:35:30 +0200

    worked on Work view; added jQuery slider for slides

--- source/javascripts/work.js
+++ source/javascripts/work.js
@@ -1,3 +1,66 @@
+//slider functionality (wrapped in my own jQuery function)
+(function($) {
+  $.fn.mySlider = function() {
+    return this.each(function() {
+      var slides;
+      var amount;
+      var i;
+      var next;
+      var previous;
+      var next;
+      var stoptimeout;
+
+      function run() {
+        //hiding previous img and showing next
+        $(slides[i]).fadeOut(600);
+        i++;
+        if (i >= amount) i = 0;
+        $(slides[i]).fadeIn(600);
+
+        //loop
+        myTimeout = setTimeout(run, 5000);
+      };
+
+      function nextSlide() {
+        $(slides[i]).fadeOut(600);
+        i++;
+        if (i >= amount) i = 0;
+        $(slides[i]).fadeIn(600);
+        //need to clear interval too
+        clearTimeout(myTimeout);
+        myTimeout = setTimeout(run, 5000);
+      };
+
+      function previousSlide() {
+        $(slides[i]).fadeOut(600);
+        i--;
+        if (i < 0) i = amount - 1;
+        $(slides[i]).fadeIn(600);
+        //need to clear interval too
+        clearTimeout(myTimeout);
+        myTimeout = setTimeout(run, 5000);
+      };
+
+      function stopTimeout() {
+        clearTimeout(myTimeout);
+      };
+
+      slides = $('.active-slider').children();
+      amount = slides.length;
+      i = 0;
+      next = $('.next');
+      previous = $('.prev');
+      stoptimeout = $('#close-expanded');
+
+      $(next).click(nextSlide);
+      $(previous).click(previousSlide);
+      $(stoptimeout).click(stopTimeout);
+
+      myTimeout = setTimeout(run, 5000);
+    });
+  };
+}(jQuery));
+
 $(document).ready(function() {
   $('#fullpage').fullpage({
     anchors: ['about-section'],
@@ -17,7 +80,40 @@ $(document).ready(function() {
   });
 
   //close project expanded view
-  $("#project-expanded").on("click", function() {
-    $(this).fadeOut();
+  $("#close-expanded").on("click", function() {
+    $(".project-details").slideUp();
+    $(this).closest("#project-expanded").delay(400).fadeOut();
+  });
+
+  //expand each project view on a click of a respective button
+  $("button#wdfs-button").on("click", function() {
+    $("#wdfs-details").delay(400).slideDown();
+  });
+
+  $("button#todo-button").on("click", function() {
+    $("#todo-details").delay(400).slideDown();
+  });
+
+  $("button#postit-button").on("click", function() {
+    $("#postit-details").delay(400).slideDown();
+  });
+
+  //execute slider
+  $('#wdfs-button').click(function() {
+    $('.active-slider').removeClass('active-slider');
+    $('#wdfs-slideshow').addClass('active-slider');
+    $('.active-slider').closest('.project-details').mySlider();
+  });
+
+  $('#todo-button').click(function() {
+    $('.active-slider').removeClass('active-slider');
+    $('#todo-slideshow').addClass('active-slider');
+    $('.active-slider').closest('.project-details').mySlider();
+  });
+
+  $('#postit-button').click(function() {
+    $('.active-slider').removeClass('active-slider');
+    $('#postit-slideshow').addClass('active-slider');
+    $('.active-slider').closest('.project-details').mySlider();
   });
 });
